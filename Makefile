d28066da6a120f074b97a043b745bf2cdfaf1e58
wrobell <wrobell@pld-linux.org>
2009-09-28 05:33:25 +0100

    - added setuptools command to build documentation with both epydoc and   sphinx

--- Makefile
+++ Makefile
@@ -1,11 +1,3 @@
-EPYDOC_ARGS=--docformat restructuredtext --parse-only -v --html --no-frames
-build-doc:
-	mkdir -p doc/_build/html/api
-	rm -f doc/extapi.pyc doc/extapi.pyo
-	#gaphorconvert -f png -v -u -d dino-doc/diagrams doc/design/dino.gaphor
-	epydoc $(EPYDOC_ARGS) -o doc/_build/html/api -n "Kenozooid Project" kenozooid
-	sphinx-build doc doc/_build/html
-
 upload-doc:
 	rsync --delete-after \
 		--delete-excluded \
--- setup.py
+++ setup.py
@@ -4,6 +4,73 @@ from setuptools import setup, find_packages
 
 version = __import__('kenozooid').__version__
 
+from distutils.cmd import Command
+
+class EpydocBuildDoc(Command):
+    description = 'Builds the documentation with epydoc'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        epydoc_conf = 'epydoc.conf'
+
+        try:
+            import sys
+            from epydoc import cli
+            old_argv = sys.argv[1:]
+            sys.argv[1:] = [
+                '--config=%s' % epydoc_conf,
+                '--no-private',
+                '--simple-term',
+                '--verbose'
+            ]
+            cli.cli()
+            sys.argv[1:] = old_argv
+
+        except ImportError:
+            print 'epydoc not installed, skipping API documentation'
+
+
+
+class build_doc(Command):
+    description = 'Builds the documentation'
+    user_options = []
+
+    def __init__(self, dist):
+        self.epydoc = EpydocBuildDoc(dist)
+        self.sphinx = None
+        try:
+            from sphinx.setup_command import BuildDoc as SphinxBuildDoc
+            self.sphinx = SphinxBuildDoc(dist)
+        except ImportError:
+            print 'sphinx not installed, skipping user documentation'
+
+        Command.__init__(self, dist)
+
+
+    def initialize_options(self):
+        if self.sphinx:
+            self.sphinx.initialize_options()
+        self.epydoc.initialize_options()
+
+
+    def finalize_options(self):
+        if self.sphinx:
+            self.sphinx.finalize_options()
+        self.epydoc.finalize_options()
+
+
+    def run(self):
+        if self.sphinx:
+            self.sphinx.run()
+        self.epydoc.run()
+
+
 setup(
     name='kenozooid',
     version=version,
@@ -38,5 +105,6 @@ computers.
         ],
     },
     test_suite='nose.collector',
+    cmdclass={'build_epydoc': EpydocBuildDoc, 'build_doc': build_doc},
 )
 
