4438b93182308dea698d34e66fbb2079fc9df36b
wrobell <wrobell@pld-linux.org>
2009-10-09 00:00:38 +0100

    - separate user, library and hacking documentation

--- doc/conf.py
+++ doc/conf.py
@@ -4,12 +4,13 @@ import os.path
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('doc'))
 
+import kenozooid
+
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
     'extapi']
 project = 'Kenozooid'
 source_suffix = '.txt'
 master_doc = 'index'
 
-version = '0.1'
-release = '0.1'
-
+version = kenozooid.__version__
+release = kenozooid.__version__
