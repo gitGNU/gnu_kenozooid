1c12c8d2e3979c1f598dbee633f52d27c79c00be
wrobell <wrobell@pld-linux.org>
2011-09-06 18:57:40 +0100

    - approximate deco ceiling using left-continuous function (deco ceiling   info information is calculated due to past data)

--- kenozooid/plot.py
+++ kenozooid/plot.py
@@ -142,7 +142,8 @@ plot(NA, xlim=xlim, ylim=ylim,
 
 # deco info
 if (any(!is.na(dp$deco_time))) {
-    deco_depth = approxfun(dp$time, dp$deco_depth, method='constant')(dp$time)
+    deco_depth = approxfun(dp$time, dp$deco_depth,
+        method='constant', f=1)(dp$time)
 
     n = length(dp$time)
     dc = rep(rgb(0.90, 0.90, 1.0), n - 1)
