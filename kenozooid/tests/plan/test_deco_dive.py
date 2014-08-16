0e0cd96e07ea1a0c99479152e616c5320d7b2fb8
wrobell <wrobell@pld-linux.org>
2014-08-16 14:28:34 +0100

    - s/assertEquals/assertEqual/

--- kenozooid/tests/plan/test_deco_dive.py
+++ kenozooid/tests/plan/test_deco_dive.py
@@ -48,33 +48,33 @@ class DecoDivePlannerTestCase(unittest.TestCase):
         plan = DivePlan()
         plan_deco_dive(plan, gas_list, 45, 35)
 
-        self.assertEquals(4, len(plan.profiles))
+        self.assertEqual(4, len(plan.profiles))
 
         t = ProfileType
         expected = [t.PLANNED, t.EXTENDED, t.LOST_GAS, t.EXTENDED_LOST_GAS]
         types = [p.type for p in plan.profiles]
-        self.assertEquals(expected, types)
+        self.assertEqual(expected, types)
 
-        self.assertEquals(45, plan.profiles[0].depth)
-        self.assertEquals(35, plan.profiles[0].time)
-        self.assertEquals(50, plan.profiles[1].depth)
-        self.assertEquals(38, plan.profiles[1].time)
-        self.assertEquals(45, plan.profiles[2].depth)
-        self.assertEquals(35, plan.profiles[2].time)
-        self.assertEquals(50, plan.profiles[3].depth)
-        self.assertEquals(38, plan.profiles[3].time)
+        self.assertEqual(45, plan.profiles[0].depth)
+        self.assertEqual(35, plan.profiles[0].time)
+        self.assertEqual(50, plan.profiles[1].depth)
+        self.assertEqual(38, plan.profiles[1].time)
+        self.assertEqual(45, plan.profiles[2].depth)
+        self.assertEqual(35, plan.profiles[2].time)
+        self.assertEqual(50, plan.profiles[3].depth)
+        self.assertEqual(38, plan.profiles[3].time)
 
         # check deco gas
-        self.assertEquals([ean50, ean80], plan.profiles[0].gas_list.deco_gas)
-        self.assertEquals([ean50, ean80], plan.profiles[1].gas_list.deco_gas)
-        self.assertEquals([], plan.profiles[2].gas_list.deco_gas)
-        self.assertEquals([], plan.profiles[3].gas_list.deco_gas)
+        self.assertEqual([ean50, ean80], plan.profiles[0].gas_list.deco_gas)
+        self.assertEqual([ean50, ean80], plan.profiles[1].gas_list.deco_gas)
+        self.assertEqual([], plan.profiles[2].gas_list.deco_gas)
+        self.assertEqual([], plan.profiles[3].gas_list.deco_gas)
 
         # check bottom gas
-        self.assertEquals(air, plan.profiles[0].gas_list.bottom_gas)
-        self.assertEquals(air, plan.profiles[1].gas_list.bottom_gas)
-        self.assertEquals(air, plan.profiles[2].gas_list.bottom_gas)
-        self.assertEquals(air, plan.profiles[3].gas_list.bottom_gas)
+        self.assertEqual(air, plan.profiles[0].gas_list.bottom_gas)
+        self.assertEqual(air, plan.profiles[1].gas_list.bottom_gas)
+        self.assertEqual(air, plan.profiles[2].gas_list.bottom_gas)
+        self.assertEqual(air, plan.profiles[3].gas_list.bottom_gas)
 
 
     @mock.patch('decotengu.create')
@@ -105,15 +105,15 @@ class DecoDivePlannerTestCase(unittest.TestCase):
         args = engine.add_gas.call_args_list
         print(dir(args[0]))
         # check travel gas
-        self.assertEquals(((0, 32, 0), {'travel': True}), args[0])
+        self.assertEqual(((0, 32, 0), {'travel': True}), args[0])
         # check bottom gas
-        self.assertEquals(((33, 27, 0),), args[1])
+        self.assertEqual(((33, 27, 0),), args[1])
         # check deco gas
-        self.assertEquals(((22, 50, 0),), args[2])
-        self.assertEquals(((10, 80, 0),), args[3])
+        self.assertEqual(((22, 50, 0),), args[2])
+        self.assertEqual(((10, 80, 0),), args[3])
 
         # check deco stops are returned
-        self.assertEquals(engine.deco_table, stops)
+        self.assertEqual(engine.deco_table, stops)
 
         self.assertFalse(engine.last_stop_6m)
         self.assertEqual(0.3, engine.model.gf_low)
@@ -175,22 +175,22 @@ class DecoDivePlannerTestCase(unittest.TestCase):
         legs = dive_legs(profile, stops, descent_rate)
         slate = dive_slate(profile, stops, legs, descent_rate)
 
-        self.assertEquals(8, len(slate), slate)
+        self.assertEqual(8, len(slate), slate)
 
-        self.assertEquals((45, None, 35, ean27), slate[0], slate)
-        self.assertEquals((22, None, 37, ean50), slate[1], slate)
+        self.assertEqual((45, None, 35, ean27), slate[0], slate)
+        self.assertEqual((22, None, 37, ean50), slate[1], slate)
         # runtime = 38.7
-        self.assertEquals((18, 1, 39, None), slate[2], slate)
+        self.assertEqual((18, 1, 39, None), slate[2], slate)
         # runtime = 38.7 + 1.3
-        self.assertEquals((15, 1, 40, None), slate[3], slate)
+        self.assertEqual((15, 1, 40, None), slate[3], slate)
         # runtime = 40 + 2.3
-        self.assertEquals((12, 2, 42, None), slate[4], slate)
+        self.assertEqual((12, 2, 42, None), slate[4], slate)
         # runtime = 42.3 + 3.3
-        self.assertEquals((9, 3, 46, ean80), slate[5], slate)
+        self.assertEqual((9, 3, 46, ean80), slate[5], slate)
         # runtime = 45.6 + 5.3
-        self.assertEquals((6, 5, 51, None), slate[6], slate)
+        self.assertEqual((6, 5, 51, None), slate[6], slate)
         # runtime = 50.9 + 0.6 = 51.4999...
-        self.assertEquals((0, None, 51, None), slate[7], slate)
+        self.assertEqual((0, None, 51, None), slate[7], slate)
 
 
     def test_dive_slate_travel(self):
@@ -226,10 +226,10 @@ class DecoDivePlannerTestCase(unittest.TestCase):
         legs = dive_legs(profile, stops, descent_rate)
         slate = dive_slate(profile, stops, legs, descent_rate)
 
-        self.assertEquals((0, None, 0, ean32), slate[0], slate)
-        self.assertEquals((33, None, 2, ean30), slate[1], slate)
-        self.assertEquals((37, None, 2, ean27), slate[2], slate)
-        self.assertEquals((45, None, 35, None), slate[3], slate)
+        self.assertEqual((0, None, 0, ean32), slate[0], slate)
+        self.assertEqual((33, None, 2, ean30), slate[1], slate)
+        self.assertEqual((37, None, 2, ean27), slate[2], slate)
+        self.assertEqual((45, None, 35, None), slate[3], slate)
 
 
     def test_depth_to_time_ascent(self):
@@ -239,7 +239,7 @@ class DecoDivePlannerTestCase(unittest.TestCase):
         The time is expected to be > 0.
         """
         t = depth_to_time(45, 10, 10)
-        self.assertEquals(3.5, t)
+        self.assertEqual(3.5, t)
 
 
     def test_depth_to_time_descent(self):
@@ -249,7 +249,7 @@ class DecoDivePlannerTestCase(unittest.TestCase):
         The time is expected to be > 0.
         """
         t = depth_to_time(0, 45, 20)
-        self.assertEquals(2.25, t)
+        self.assertEqual(2.25, t)
 
 
     def test_sum_deco_time(self):
@@ -305,14 +305,14 @@ class GasMixParserTestCase(unittest.TestCase):
         Test parsing air gas mix
         """
         m = parse_gas('air')
-        self.assertEquals(21, m.o2)
-        self.assertEquals(0, m.he)
-        self.assertEquals(66, m.depth)
+        self.assertEqual(21, m.o2)
+        self.assertEqual(0, m.he)
+        self.assertEqual(66, m.depth)
 
         m = parse_gas('air@0')
-        self.assertEquals(21, m.o2)
-        self.assertEquals(0, m.he)
-        self.assertEquals(0, m.depth)
+        self.assertEqual(21, m.o2)
+        self.assertEqual(0, m.he)
+        self.assertEqual(0, m.depth)
 
 
     def test_parse_gas_o2(self):
@@ -320,9 +320,9 @@ class GasMixParserTestCase(unittest.TestCase):
         Test parsing o2 gas mix
         """
         m = parse_gas('o2')
-        self.assertEquals(100, m.o2)
-        self.assertEquals(0, m.he)
-        self.assertEquals(6, m.depth)
+        self.assertEqual(100, m.o2)
+        self.assertEqual(0, m.he)
+        self.assertEqual(6, m.depth)
 
 
     def test_parse_gas_ean(self):
@@ -330,14 +330,14 @@ class GasMixParserTestCase(unittest.TestCase):
         Test parsing EAN gas mix
         """
         m = parse_gas('EAN50')
-        self.assertEquals(50, m.o2)
-        self.assertEquals(0, m.he)
-        self.assertEquals(22, m.depth)
+        self.assertEqual(50, m.o2)
+        self.assertEqual(0, m.he)
+        self.assertEqual(22, m.depth)
 
         m = parse_gas('ean32@21')
-        self.assertEquals(32, m.o2)
-        self.assertEquals(0, m.he)
-        self.assertEquals(21, m.depth)
+        self.assertEqual(32, m.o2)
+        self.assertEqual(0, m.he)
+        self.assertEqual(21, m.depth)
 
 
     def test_parse_trimix(self):
@@ -345,14 +345,14 @@ class GasMixParserTestCase(unittest.TestCase):
         Test parsing trimix gas
         """
         m = parse_gas('TX21/33@10')
-        self.assertEquals(21, m.o2)
-        self.assertEquals(33, m.he)
-        self.assertEquals(10, m.depth)
+        self.assertEqual(21, m.o2)
+        self.assertEqual(33, m.he)
+        self.assertEqual(10, m.depth)
 
         m = parse_gas('TX17/18')
-        self.assertEquals(17, m.o2)
-        self.assertEquals(18, m.he)
-        self.assertEquals(84, m.depth)
+        self.assertEqual(17, m.o2)
+        self.assertEqual(18, m.he)
+        self.assertEqual(84, m.depth)
 
     # TODO: 'TX17/18|2x12', 'TX21/33@10'
 
@@ -375,9 +375,9 @@ class GasMixParserTestCase(unittest.TestCase):
         air = gas(21, 0)
         ean50 = gas(50, 0, 22)
         gas_list = parse_gas_list('air@0', 'ean50')
-        self.assertEquals([], gas_list.travel_gas)
-        self.assertEquals(air, gas_list.bottom_gas)
-        self.assertEquals([ean50], gas_list.deco_gas)
+        self.assertEqual([], gas_list.travel_gas)
+        self.assertEqual(air, gas_list.bottom_gas)
+        self.assertEqual([ean50], gas_list.deco_gas)
 
 
 
@@ -402,16 +402,16 @@ class DiveLegsTestCase(unittest.TestCase):
 
         legs = dive_legs(profile, stops, 20)
 
-        self.assertEquals(3 + 10, len(legs))
-        self.assertEquals((0, 60, 3, air, False), legs[0])
-        self.assertEquals((60, 60, 22, air, False), legs[1])
-        self.assertEquals((60, 18, 4.2, air, False), legs[2])
+        self.assertEqual(3 + 10, len(legs))
+        self.assertEqual((0, 60, 3, air, False), legs[0])
+        self.assertEqual((60, 60, 22, air, False), legs[1])
+        self.assertEqual((60, 18, 4.2, air, False), legs[2])
 
         deco_legs = legs[3::2]
         for s, l in zip(stops, deco_legs):
-            self.assertEquals((s.depth, s.depth, s.time, air, True), l)
+            self.assertEqual((s.depth, s.depth, s.time, air, True), l)
 
-        self.assertEquals((6, 0, 0.6, air, True), legs[-1])
+        self.assertEqual((6, 0, 0.6, air, True), legs[-1])
 
 
     def test_dive_legs_deco_gas(self):
@@ -435,24 +435,24 @@ class DiveLegsTestCase(unittest.TestCase):
 
         legs = dive_legs(profile, stops, 20)
 
-        self.assertEquals(4 + 10, len(legs))
-        self.assertEquals((0, 60, 3, air, False), legs[0])
-        self.assertEquals((60, 60, 22, air, False), legs[1])
-        self.assertEquals((60, 22, 3.8, air, False), legs[2])
-        self.assertEquals((22, 18, 0.4, ean50, False), legs[3])
+        self.assertEqual(4 + 10, len(legs))
+        self.assertEqual((0, 60, 3, air, False), legs[0])
+        self.assertEqual((60, 60, 22, air, False), legs[1])
+        self.assertEqual((60, 22, 3.8, air, False), legs[2])
+        self.assertEqual((22, 18, 0.4, ean50, False), legs[3])
 
-        self.assertEquals((18, 18, 1, ean50, True), legs[4])
-        self.assertEquals((18, 15, 0.3, ean50, True), legs[5])
-        self.assertEquals((15, 15, 1, ean50, True), legs[6])
-        self.assertEquals((15, 12, 0.3, ean50, True), legs[7])
+        self.assertEqual((18, 18, 1, ean50, True), legs[4])
+        self.assertEqual((18, 15, 0.3, ean50, True), legs[5])
+        self.assertEqual((15, 15, 1, ean50, True), legs[6])
+        self.assertEqual((15, 12, 0.3, ean50, True), legs[7])
 
-        self.assertEquals((12, 12, 2, ean50, True), legs[8])
-        self.assertEquals((12, 9, 0.3, ean50, True), legs[9])
-        self.assertEquals((9, 9, 3, ean80, True), legs[10])
-        self.assertEquals((9, 6, 0.3, ean80, True), legs[11])
+        self.assertEqual((12, 12, 2, ean50, True), legs[8])
+        self.assertEqual((12, 9, 0.3, ean50, True), legs[9])
+        self.assertEqual((9, 9, 3, ean80, True), legs[10])
+        self.assertEqual((9, 6, 0.3, ean80, True), legs[11])
 
-        self.assertEquals((6, 6, 5, o2, True), legs[-2])
-        self.assertEquals((6, 0, 0.6, o2, True), legs[-1])
+        self.assertEqual((6, 6, 5, o2, True), legs[-2])
+        self.assertEqual((6, 0, 0.6, o2, True), legs[-1])
 
 
     def test_dive_legs_travel_gas(self):
@@ -476,18 +476,18 @@ class DiveLegsTestCase(unittest.TestCase):
         profile = DiveProfile(ProfileType.PLANNED, gas_list, 60, 25)
         legs = dive_legs(profile, stops, 20)
 
-        self.assertEquals(5 + 10, len(legs))
-        self.assertEquals((0, 30, 1.5, ean36, False), legs[0])
-        self.assertEquals((30, 40, 0.5, ean26, False), legs[1])
-        self.assertEquals((40, 60, 1, air, False), legs[2])
-        self.assertEquals((60, 60, 22, air, False), legs[3])
-        self.assertEquals((60, 18, 4.2, air, False), legs[4])
+        self.assertEqual(5 + 10, len(legs))
+        self.assertEqual((0, 30, 1.5, ean36, False), legs[0])
+        self.assertEqual((30, 40, 0.5, ean26, False), legs[1])
+        self.assertEqual((40, 60, 1, air, False), legs[2])
+        self.assertEqual((60, 60, 22, air, False), legs[3])
+        self.assertEqual((60, 18, 4.2, air, False), legs[4])
 
         deco_legs = legs[5::2]
         for s, l in zip(stops, deco_legs):
-            self.assertEquals((s.depth, s.depth, s.time, air, True), l)
+            self.assertEqual((s.depth, s.depth, s.time, air, True), l)
 
-        self.assertEquals((6, 0, 0.6, air, True), legs[-1])
+        self.assertEqual((6, 0, 0.6, air, True), legs[-1])
 
 
     def test_dive_legs_overhead_deco(self):
@@ -513,7 +513,7 @@ class DiveLegsTestCase(unittest.TestCase):
             # ...
         ]
         oh_legs = dive_legs_overhead(gas_list, legs)
-        self.assertEquals(legs[:3], oh_legs)
+        self.assertEqual(legs[:3], oh_legs)
 
 
     def test_dive_legs_overhead_switch(self):
@@ -536,7 +536,7 @@ class DiveLegsTestCase(unittest.TestCase):
             (6, 0, 1, ean50, True),
         ]
         oh_legs = dive_legs_overhead(gas_list, legs)
-        self.assertEquals(legs[:3], oh_legs)
+        self.assertEqual(legs[:3], oh_legs)
 
 
 
@@ -567,9 +567,9 @@ class GasMixConsumptionTestCase(unittest.TestCase):
 
         cons = gas_volume(gas_list, legs, 30)
 
-        self.assertEquals(2, len(cons))
-        self.assertEquals(3795, cons[air])
-        self.assertEquals(445.5, cons[ean50])
+        self.assertEqual(2, len(cons))
+        self.assertEqual(3795, cons[air])
+        self.assertEqual(445.5, cons[ean50])
 
 
     def test_min_bottom_gas(self):
@@ -594,8 +594,8 @@ class GasMixConsumptionTestCase(unittest.TestCase):
 
         vol = min_gas_volume(gas_list, legs, 30)
 
-        self.assertEquals(3795 * 1.5, vol[air])
-        self.assertEquals(445.5 * 1.5, vol[ean50])
+        self.assertEqual(3795 * 1.5, vol[air])
+        self.assertEqual(445.5 * 1.5, vol[ean50])
 
 
     def test_gas_vol_info(self):
@@ -612,8 +612,8 @@ class GasMixConsumptionTestCase(unittest.TestCase):
         min_gas_vol = OrderedDict(mixes)
 
         info = gas_vol_info(gas_vol, min_gas_vol)
-        self.assertEquals(info[0], 'Gas mix Air volume OK.')
-        self.assertEquals(info[1], 'Gas mix EAN50 volume OK.')
+        self.assertEqual(info[0], 'Gas mix Air volume OK.')
+        self.assertEqual(info[1], 'Gas mix EAN50 volume OK.')
 
 
     def test_gas_vol_info_warn(self):
@@ -630,8 +630,8 @@ class GasMixConsumptionTestCase(unittest.TestCase):
         min_gas_vol = OrderedDict(mixes)
 
         info = gas_vol_info(gas_vol, min_gas_vol)
-        self.assertEquals(info[0], 'WARN: Gas mix Air volume NOT OK.')
-        self.assertEquals(info[1], 'WARN: Gas mix EAN50 volume NOT OK.')
+        self.assertEqual(info[0], 'WARN: Gas mix Air volume NOT OK.')
+        self.assertEqual(info[1], 'WARN: Gas mix EAN50 volume NOT OK.')
 
 
 # vim: sw=4:et:ai
