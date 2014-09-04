e558a9439a1b23dc7c3b995d6dc07565391fb043
wrobell <wrobell@pld-linux.org>
2014-09-04 03:44:37 +0100

    - documentation configuration file cleanup

--- doc/conf.py
+++ doc/conf.py
@@ -6,8 +6,10 @@ sys.path.append(os.path.abspath('doc'))
 
 import kenozooid
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-    'sphinx.ext.viewcode', 'extapi']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+    'sphinx.ext.viewcode', 'extapi'
+]
 project = 'Kenozooid'
 source_suffix = '.rst'
 master_doc = 'index'
@@ -18,11 +20,5 @@ copyright = 'Kenozooid team'
 
 epub_basename = 'Kenozooid - {}'.format(version)
 epub_author = 'Kenozooid team'
-
-
-# see https://bitbucket.org/birkenfeld/sphinx/issue/635/no-attribute-next
-import sphinx.jinja2glue
-if not hasattr(sphinx.jinja2glue.idgen, "next"):
-      sphinx.jinja2glue.idgen.next = sphinx.jinja2glue.idgen.__next__
 
 # vim: sw=4:et:ai
